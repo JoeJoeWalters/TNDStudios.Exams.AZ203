dde00c602109f72d9cf5d8c1fa030badc2ffc5eb
TNDStudios <info@thenakeddeveloper.com>
2019-10-06 00:42:12 +0100

    Swapped out API App for regular Web App

--- TNDStudios.Exams.AZ203/WebApps/Program.cs
+++ TNDStudios.Exams.AZ203/WebApps/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace APIApps
+namespace WebApps
 {
     public class Program
     {
