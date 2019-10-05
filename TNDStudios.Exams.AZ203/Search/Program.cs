20d53c16a859c9ab156c8a9c77bab24f63dca1c5
TNDStudios <info@thenakeddeveloper.com>
2019-10-05 17:15:34 +0100

    Added Events, Search and ServiceBus projects

---
+++ TNDStudios.Exams.AZ203/Search/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
