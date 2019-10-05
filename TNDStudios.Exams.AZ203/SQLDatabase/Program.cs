95f4283eb1c1ceda37dda9297d8ab7f2a2d4c448
TNDStudios <info@thenakeddeveloper.com>
2019-10-05 16:04:50 +0100

    Starting population of project types

---
+++ TNDStudios.Exams.AZ203/SQLDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQLDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
