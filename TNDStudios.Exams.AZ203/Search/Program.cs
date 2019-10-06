35da5490c4b96416f28ed3bf3557c37775162b71
TNDStudios <info@thenakeddeveloper.com>
2019-10-07 00:33:24 +0100

    Really added all the files this time

--- TNDStudios.Exams.AZ203/Search/Program.cs
+++ TNDStudios.Exams.AZ203/Search/Program.cs
@@ -1,12 +1,355 @@
-﻿using System;
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Spatial;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading;
 
+// https://docs.microsoft.com/en-us/azure/search/search-howto-dotnet-sdk
 namespace Search
 {
+    public partial class Room
+    {
+        [IsSearchable]
+        [Analyzer(AnalyzerName.AsString.EnMicrosoft)]
+        public string Description { get; set; }
+
+        [IsSearchable]
+        [Analyzer(AnalyzerName.AsString.FrMicrosoft)]
+        [JsonProperty("Description_fr")]
+        public string DescriptionFr { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string Type { get; set; }
+
+        [IsFilterable, IsFacetable]
+        public double? BaseRate { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string BedOptions { get; set; }
+
+        [IsFilterable, IsFacetable]
+        public int SleepsCount { get; set; }
+
+        [IsFilterable, IsFacetable]
+        public bool? SmokingAllowed { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string[] Tags { get; set; }
+    }
+
+    public partial class Address
+    {
+        [IsSearchable]
+        public string StreetAddress { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+        public string City { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+        public string StateProvince { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+        public string PostalCode { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+        public string Country { get; set; }
+    }
+
+    public partial class Hotel
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        [IsFilterable]
+        public string HotelId { get; set; }
+
+        [IsSearchable, IsSortable]
+        public string HotelName { get; set; }
+
+        [IsSearchable]
+        [Analyzer(AnalyzerName.AsString.EnLucene)]
+        public string Description { get; set; }
+
+        [IsSearchable]
+        [Analyzer(AnalyzerName.AsString.FrLucene)]
+        [JsonProperty("Description_fr")]
+        public string DescriptionFr { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+        public string Category { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string[] Tags { get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public bool? ParkingIncluded { get; set; }
+
+        // SmokingAllowed reflects whether any room in the hotel allows smoking.
+        // The JsonIgnore attribute indicates that a field should not be created 
+        // in the index for this property and it will only be used by code in the client.
+        [JsonIgnore]
+        public bool? SmokingAllowed => (Rooms != null) ? Array.Exists(Rooms, element => element.SmokingAllowed == true) : (bool?)null;
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public DateTimeOffset? LastRenovationDate { get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public double? Rating { get; set; }
+
+        public Address Address { get; set; }
+
+        [IsFilterable, IsSortable]
+        public GeographyPoint Location { get; set; }
+
+        public Room[] Rooms { get; set; }
+    }
+
     class Program
     {
+        private static SearchServiceClient CreateSearchServiceClient(IConfigurationRoot configuration)
+        {
+            string searchServiceName = configuration["SearchServiceName"];
+            string adminApiKey = configuration["SearchServiceAdminApiKey"];
+
+            SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
+            return serviceClient;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            //IConfigurationRoot configuration = builder.Build();
+
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+            IConfigurationRoot configuration = builder.Build();
+
+            SearchServiceClient serviceClient = CreateSearchServiceClient(configuration);
+
+            string indexName = configuration["SearchIndexName"];
+
+            Console.WriteLine("{0}", "Deleting index\n");
+            DeleteIndexIfExists(indexName, serviceClient);
+
+            Console.WriteLine("{0}", "Creating index\n");
+            CreateIndex(indexName, serviceClient);
+
+            ISearchIndexClient indexClient = serviceClient.Indexes.GetClient(indexName);
+
+            Console.WriteLine("{0}", "Uploading documents\n");
+            UploadDocuments(indexClient);
+
+            ISearchIndexClient indexClientForQueries = CreateSearchIndexClient(indexName, configuration);
+
+            RunQueries(indexClientForQueries);
+
+            Console.WriteLine("{0}", "Complete.  Press any key to end application\n");
+            Console.ReadKey();
+        }
+        
+        private static SearchIndexClient CreateSearchIndexClient(string indexName, IConfigurationRoot configuration)
+        {
+            string searchServiceName = configuration["SearchServiceName"];
+            string queryApiKey = configuration["SearchServiceQueryApiKey"];
+
+            SearchIndexClient indexClient = new SearchIndexClient(searchServiceName, indexName, new SearchCredentials(queryApiKey));
+            return indexClient;
+        }
+
+        private static void RunQueries(ISearchIndexClient indexClient)
+        {
+            SearchParameters parameters;
+            DocumentSearchResult<Hotel> results;
+
+            Console.WriteLine("Search the entire index for the term 'motel' and return only the HotelName field:\n");
+
+            parameters =
+                new SearchParameters()
+                {
+                    Select = new[] { "HotelName" }
+                };
+
+            results = indexClient.Documents.Search<Hotel>("motel", parameters);
+
+            WriteDocuments(results);
+
+            Console.Write("Apply a filter to the index to find hotels with a room cheaper than $100 per night, ");
+            Console.WriteLine("and return the hotelId and description:\n");
+
+            parameters =
+                new SearchParameters()
+                {
+                    Filter = "Rooms/any(r: r/BaseRate lt 100)",
+                    Select = new[] { "HotelId", "Description" }
+                };
+
+            results = indexClient.Documents.Search<Hotel>("*", parameters);
+
+            WriteDocuments(results);
+
+            Console.Write("Search the entire index, order by a specific field (lastRenovationDate) ");
+            Console.Write("in descending order, take the top two results, and show only hotelName and ");
+            Console.WriteLine("lastRenovationDate:\n");
+
+            parameters =
+                new SearchParameters()
+                {
+                    OrderBy = new[] { "LastRenovationDate desc" },
+                    Select = new[] { "HotelName", "LastRenovationDate" },
+                    Top = 2
+                };
+
+            results = indexClient.Documents.Search<Hotel>("*", parameters);
+
+            WriteDocuments(results);
+
+            Console.WriteLine("Search the entire index for the term 'hotel':\n");
+
+            parameters = new SearchParameters();
+            results = indexClient.Documents.Search<Hotel>("hotel", parameters);
+
+            WriteDocuments(results);
+        }
+
+        private static void WriteDocuments(DocumentSearchResult<Hotel> searchResults)
+        {
+            foreach (SearchResult<Hotel> result in searchResults.Results)
+            {
+                Console.WriteLine(result.Document);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void DeleteIndexIfExists(string indexName, SearchServiceClient serviceClient)
+        {
+            if (serviceClient.Indexes.Exists(indexName))
+            {
+                serviceClient.Indexes.Delete(indexName);
+            }
+        }
+
+        private static void CreateIndex(string indexName, SearchServiceClient serviceClient)
+        {
+            var definition = new Microsoft.Azure.Search.Models.Index()
+            {
+                Name = indexName,
+                Fields = FieldBuilder.BuildForType<Hotel>()
+            };
+
+            serviceClient.Indexes.Create(definition);
+        }
+
+        private static void UploadDocuments(ISearchIndexClient indexClient)
+        {
+            var hotels = new Hotel[]
+            {
+        new Hotel()
+        {
+            HotelId = "1",
+            HotelName = "Secret Point Motel",
+
+            Address = new Address()
+            {
+                StreetAddress = "677 5th Ave",
+
+            },
+            Rooms = new Room[]
+            {
+                new Room()
+                {
+                    Description = "Budget Room, 1 Queen Bed (Cityside)",
+
+                },
+                new Room()
+                {
+                    Description = "Budget Room, 1 King Bed (Mountain View)",
+
+                },
+                new Room()
+                {
+                    Description = "Deluxe Room, 2 Double Beds (City View)",
+
+                }
+            }
+        },
+        new Hotel()
+        {
+            HotelId = "2",
+            HotelName = "Twin Dome Motel",
+            Address = new Address()
+            {
+                StreetAddress = "140 University Town Center Dr"
+            },
+            Rooms = new Room[]
+            {
+                new Room()
+                {
+                    Description = "Suite, 2 Double Beds (Mountain View)",
+
+                },
+                new Room()
+                {
+                    Description = "Standard Room, 1 Queen Bed (City View)",
+
+                },
+                new Room()
+                {
+                    Description = "Budget Room, 1 King Bed (Waterfront View)",
+
+                }
+            }
+                },
+        new Hotel()
+        {
+            HotelId = "3",
+            HotelName = "Triple Landscape Hotel",
+
+            Address = new Address()
+            {
+                StreetAddress = "3393 Peachtree Rd",
+
+            },
+            Rooms = new Room[]
+            {
+                new Room()
+                {
+                    Description = "Standard Room, 2 Queen Beds (Amenities)",
+
+                },
+                new Room ()
+                {
+                    Description = "Standard Room, 2 Double Beds (Waterfront View)",
+
+                },
+                new Room()
+                {
+                    Description = "Deluxe Room, 2 Double Beds (Cityside)",
+
+                }
+            }
+        }
+    };
+
+            var batch = IndexBatch.Upload(hotels);
+
+            try
+            {
+                indexClient.Documents.Index(batch);
+            }
+            catch (IndexBatchException e)
+            {
+                // Sometimes when your Search service is under load, indexing will fail for some of the documents in
+                // the batch. Depending on your application, you can take compensating actions like delaying and
+                // retrying. For this simple demo, we just log the failed document keys and continue.
+                Console.WriteLine(
+                    "Failed to index some of the documents: {0}",
+                    String.Join(", ", e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key)));
+            }
+
+            Console.WriteLine("Waiting for documents to be indexed\n");
+            Thread.Sleep(2000);
         }
     }
 }
